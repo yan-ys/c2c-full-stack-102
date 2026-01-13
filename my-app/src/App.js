2e91e659f63ae1de8cf2f8ad6ed877729df2d197
yan-ys <songdf2@gmail.com>
2026-01-13 00:06:15 +0000

    added favorite food heading

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Top three snacks I would recomend</h1>
+      <title>chips, cookies, oranges </title>
+    </div>
+  );
+}
+
+export default App;
